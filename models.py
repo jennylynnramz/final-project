01488f3e240ceaaad2445fa82b1b8ce31606cd0e
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-08-14 16:39:11 -0700

    another capitalization error

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Input_Results(db.Model):
-    __tablename__ = 'input_results'
+    __tablename__ = 'Input_Results'
 
     id = db.Column(db.Integer, primary_key=True)
     user_input = db.Column(db.String(64))
